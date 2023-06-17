7e9e611e3acd5f8fe6bc33a6d8f68738e96c86d1
huttongrabiel <huttonthomas@icloud.com>
2023-06-17 11:31:36 -0700

    main: Two balls bouncing back and forth across screen

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,41 @@
 
 #include "object.h"
 
-int main() {
-    int screen_width = GetScreenWidth()/4;
-    int screen_height = GetScreenHeight()/4;
+constexpr int circle_radius = 80;
 
-    std::vector<Object::Object> active_objects;
+int main() {
+    int screen_width = 800;
+    int screen_height = 600;
 
     InitWindow(screen_width, screen_height, "Botsen");
 
     SetTargetFPS(60);
 
+    std::vector<Object::Object> active_objects;
+
     // For starting test purposes we will not render custom objects but instead
     // use raylib's circles
-    Object::ObjPos circle_1_pos = { screen_width/4, screen_height/2};
-    Object::ObjPos circle_2_pos = { screen_width*(3/4), screen_height/2};
+    Object::ObjPos circle_1_pos = { 200, screen_height/2}; // Left circle
+    Object::ObjPos circle_2_pos = { screen_width-200, screen_height/2}; // Right circle
 
+    int towards = 1;
     while (!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(WHITE);
+
+        // Move the balls away from one another
+        if (circle_1_pos.x+circle_radius == screen_width && circle_2_pos.x-circle_radius == 0) {
+            towards = -1;
+        // Move the balls towards one another
+        } else if (circle_1_pos.x-circle_radius == 0 && circle_2_pos.x+circle_radius == screen_width) {
+            towards = 1;
+        }
+
+        circle_1_pos.x += towards*2;
+        circle_2_pos.x -= towards*2;
         DrawCircle(circle_1_pos.x, circle_1_pos.y, 80, RED);
         DrawCircle(circle_2_pos.x, circle_2_pos.y, 80, BLUE);
+
         EndDrawing();
     }
 
