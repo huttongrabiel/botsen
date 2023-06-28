9d331daa0af330783deee64df81f3be48219e41a
huttongrabiel <huttonthomas@icloud.com>
2023-06-28 11:21:34 -0700

    UI: Add home screen with instructions

--- src/UI.h
+++ src/UI.h
@@ -5,7 +5,7 @@
 
 namespace UI {
 
-void update_and_draw_home();
+void update_and_draw_home(const GameData::Game& active_game);
 void update_and_draw_circles(const GameData::Game& active_game, RaylibExt::raylibCircleExt& circle_1, RaylibExt::raylibCircleExt& circle_2);
 
 }
