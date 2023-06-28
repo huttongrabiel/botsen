0f2fac4febb53f85e9491a8cb3a0b76124180d86
huttongrabiel <huttonthomas@icloud.com>
2023-06-28 10:25:02 -0700

    FileLayout: Rename Scenes to UI and match namespaces

--- src/UI.h
+++ src/UI.h
@@ -3,7 +3,7 @@
 #include <RaylibCircleExt.h>
 #include <GameData.h>
 
-namespace Scenes {
+namespace UI {
 
 void update_and_draw_home();
 void update_and_draw_circles(const GameData::Game& active_game, RaylibExt::raylibCircleExt& circle_1, RaylibExt::raylibCircleExt& circle_2);
