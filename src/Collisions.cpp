cfa78a9b03afb5f57e1b62c25dbfd89b2bbb0787
huttongrabiel <huttonthomas@icloud.com>
2023-06-29 21:00:09 -0700

    Collisions: Add totally inelastic collision

--- src/Collisions.cpp
+++ src/Collisions.cpp
@@ -18,11 +18,34 @@ void simulate_elastic_collision(RaylibExt::raylibCircleExt& circle_1, RaylibExt:
     }
 }
 
-// TODO: Implement collision simulation
 void simulate_inelastic_collision(RaylibExt::raylibCircleExt& circle_1, RaylibExt::raylibCircleExt& circle_2) {}
 
-// TODO: Implement collision simulation
-void simulate_totally_inelastic_collision(RaylibExt::raylibCircleExt& circle_1, RaylibExt::raylibCircleExt& circle_2) {}
+void simulate_totally_inelastic_collision(RaylibExt::raylibCircleExt& circle_1, RaylibExt::raylibCircleExt& circle_2) {
+    // We define the objects as being of mass m1 and m2, respectively. With inital velocities of v1i and v2i respectively.
+    double m1 = circle_1.mass();
+    double m2 = circle_2.mass();
+    double v1i = circle_1.velocity();
+    double v2i = circle_2.velocity();
+
+    // m1v1i + m2v2i = (m1 + m2)vf
+    // vf = (m1v1i + m2v2i) / (m1+m2)
+    double numerator = ((m1 * v1i) + (m2 * v2i));
+    double denominator = (m1 + m2);
+    double final_velocity = numerator / denominator;
+
+    int circle_1_dir_travel = circle_1.direction();
+    int circle_2_dir_travel = circle_2.direction();
+
+    if (v2i > 0) {
+        circle_1.set_direction_of_travel(circle_2_dir_travel);
+    }
+    if (v1i > 0) {
+        circle_2.set_direction_of_travel(circle_1_dir_travel);
+    }
+
+    circle_1.set_velocity(final_velocity);
+    circle_2.set_velocity(final_velocity);
+}
 
 // Checks if a ball has hit either the left or right edge of the screen
 bool has_collided_with_edge(const RaylibExt::raylibCircleExt& circle, int screen_width) {
