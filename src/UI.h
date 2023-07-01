254b32621d74c60363075a59e16d5b7f4dba2827
huttongrabiel <huttonthomas@icloud.com>
2023-07-01 14:38:57 -0700

    UI: Add ability to select collision type to home screen
    User is able to select collision type by clicking buttons with mouse.

--- src/UI.h
+++ src/UI.h
@@ -5,7 +5,8 @@
 
 namespace UI {
 
-void update_and_draw_home(const GameData::Game& active_game);
+void update_and_draw_home(GameData::Game& active_game);
+void draw_collision_select_buttons(GameData::Game& active_game, int title_position_x, int title_position_y);
 void update_and_draw_circles(const GameData::Game& active_game, RaylibExt::raylibCircleExt& circle_1, RaylibExt::raylibCircleExt& circle_2);
 
 }
